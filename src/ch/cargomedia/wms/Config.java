757e87f9014ddac6130db43b0953680bf5576fbc
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-10-21 10:17:03 +0200

    Adjust CM endpoints

--- src/ch/cargomedia/wms/Config.java
+++ src/ch/cargomedia/wms/Config.java
@@ -3,10 +3,10 @@ package ch.cargomedia.wms;
 import com.wowza.wms.application.WMSProperties;
 
 public final class Config {
-  public static final String RPC_UNSUBSCRIBE = "CM_VideoStream_Service.unsubscribe";
-  public static final String RPC_UNPUBLISH = "CM_VideoStream_Service.unpublish";
-  public static final String RPC_SUBSCRIBE = "CM_VideoStream_Service.subscribe";
-  public static final String RPC_PUBLISH = "CM_VideoStream_Service.publish";
+  public static final String RPC_UNSUBSCRIBE = "CM_Wowza_RpcEndpoints.unsubscribe";
+  public static final String RPC_UNPUBLISH = "CM_Wowza_RpcEndpoints.unpublish";
+  public static final String RPC_SUBSCRIBE = "CM_Wowza_RpcEndpoints.subscribe";
+  public static final String RPC_PUBLISH = "CM_Wowza_RpcEndpoints.publish";
 
   private WMSProperties _properties;
 
